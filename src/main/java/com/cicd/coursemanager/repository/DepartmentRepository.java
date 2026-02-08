01e55c49abe9642a2f0369793d1f211cd5d88666
ArezoH <bhalimi@cifpfbmoll.eu>
2026-02-08 14:58:31 +0100

    feat: add JPA repositories for all entities

---
+++ src/main/java/com/cicd/coursemanager/repository/DepartmentRepository.java
@@ -0,0 +1,9 @@
+package com.cicd.coursemanager.repository;
+
+import com.cicd.coursemanager.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+}
